6d5c76fcf9424fc21b2e4533ff058f281f159e72
SingularWind <1185857606@qq.com>
2025-10-28 15:03:51 +0800

    fix project structure

--- lectures/lecture3/src/IntList.java
+++ lectures/lecture3/src/IntList.java
@@ -1,27 +1,28 @@
 public class IntList {
-    int num;
-    IntList rest;
+    public int data;
+    public IntList next;
 
-    public IntList(int num, IntList rest) {
-        this.num = num;
-        this.rest = rest;
-    }
-
-    public IntList(int num) {
-        this(num, null);
+    // Constructor to initialize the list
+    IntList(int data, IntList next) {
+        this.data = data;
+        this.next = next;
     }
 
     /** =======================
      * Size methods
      * ======================= */
-    public int recursiveSize() {
-        if (rest == null) return 1;
-        return rest.recursiveSize() + 1;
+    public int size() {
+        return recursiveSize(); // or return iterativeSize();
     }
 
-    public int iterativeSize() {
+    private int recursiveSize() {
+        if (next ==null)    return 1;
+        return 1 + next.recursiveSize();
+    }
+
+    private int iterativeSize() {
         int size = 1;
-        for (IntList p = this; p.rest != null; p = p.rest) {
+        for (IntList p = this; p != null; p = p.next) {
             size++;
         }
         return size;
@@ -30,103 +31,84 @@ public class IntList {
     /** =======================
      * Get methods
      * ======================= */
-    public int recursiveGet(int index) {
-        if (index < 0 || index >= recursiveSize()) {
-            throw new IndexOutOfBoundsException();
+    public int get(int index) {
+        if(index < 0 || index >= size()) {
+            throw new ArrayIndexOutOfBoundsException("Index: " + index + ", Size: " + size());
         }
-        if (index == 0) return num;
-        return rest.recursiveGet(index - 1);
+        return  recursiveGet(index);    // or return iterativeGet(index);
     }
 
-    public int iterativeGet(int index) {
-        if (index < 0 || index >= iterativeSize()) {
-            throw new IndexOutOfBoundsException();
+    private int recursiveGet(int index) {
+        if (index == 0) return data;
+        return next.recursiveGet(index - 1);
+    }
+
+    private int iterativeGet(int index) {
+        int size = 0;
+        for(IntList p = this; p != null; p = p.next) {
+            size++;
         }
-        IntList p = this;
-        for (int i = 0; i < index; i++, p = p.rest);
-        return p.num;
+        return size;
     }
 
     /** =======================
      * Increment methods
      * ======================= */
     public static IntList incrList(IntList L, int x) {
-        if (L == null) return null;
-        return new IntList(L.num + x, incrList(L.rest, x));
+        IntList newList = null;
+        IntList q = null;
+        for (IntList p = L; p != null; p = p.next) {
+            IntList newNode = new IntList(p.data + x, q);
+            if (newList == null) {
+                newList = newNode;
+                q = newList;
+            } else {
+                q = newNode;
+            }
+        }
+        return newList;
     }
 
     public static IntList dincrList(IntList L, int x) {
-        for (IntList p = L; p != null; p = p.rest) {
-            p.num += x;
+        for(IntList p = L; p != null; p = p.next) {
+            p.data += x;
         }
         return L;
-    }
+}
 
     /** =======================
      * Add first
      * ======================= */
-    public IntList addFirst(int x) { // functional
-        return new IntList(x, this);
+    private IntList addFirst(int x) { // functional
+        return new IntList(x, next);
     }
 
-    public void addFirstInPlace(int x) { // in-place
-        IntList temp = new IntList(num, rest);
-        num = x;
-        rest = temp;
+    private void daddFirst(int x) { // in-place
+        IntList copy = new IntList(data, next);
+        data = x;
+        next = copy;
     }
 
     /** =======================
      * Get first element
      * ======================= */
     public int getFirst() {
-        return num;
+        return data;
     }
 
     /** =======================
      * Utility: print list
      * ======================= */
     public void printList() {
-        for (IntList p = this; p != null; p = p.rest) {
-            System.out.print(p.num + " ");
+        for (IntList p = this; p != null; p = p.next) {
+            System.out.print(p.data + " ");
         }
-        System.out.println();
     }
 
     /** =======================
-     * Main demo
+     * Main demo and tests
      * ======================= */
     public static void main(String[] args) {
-        // Original list
-        IntList L = new IntList(1, new IntList(2, new IntList(3)));
-        System.out.println("Original list:");
-        L.printList(); // 1 2 3
 
-        // Size
-        System.out.println("Size (iterative): " + L.iterativeSize()); // 3
-        System.out.println("Size (recursive): " + L.recursiveSize()); // 3
-
-        // Get
-        System.out.println("Get index 1 (iterative): " + L.iterativeGet(1)); // 2
-        System.out.println("Get index 2 (recursive): " + L.recursiveGet(2)); // 3
-
-        // Increment (new list)
-        IntList L2 = IntList.incrList(L, 5);
-        System.out.println("New list after incrList +5:");
-        L2.printList(); // 6 7 8
-
-        // Increment in-place
-        IntList.dincrList(L, 10);
-        System.out.println("Original list after dincrList +10:");
-        L.printList(); // 11 12 13
-
-        // Add first (functional)
-        L = L.addFirst(0);
-        System.out.println("After addFirst(0) (functional):");
-        L.printList(); // 0 11 12 13
-
-        // Add first in-place
-        L.addFirstInPlace(-1);
-        System.out.println("After addFirstInPlace(-1) (in-place):");
-        L.printList(); // -1 0 11 12 13
     }
 }
