207c61cf8bfcd80bf98ae40954901b19417bf843
SingularWind <1185857606@qq.com>
2025-11-27 21:59:46 +0800

    doing project1A

---
+++ projects/proj1a/src/LinkedListDeque61B.java
@@ -0,0 +1,138 @@
+import java.util.List;
+
+public class LinkedListDeque61B<T> implements Deque61B<T> {
+    private  class Node {
+        T item;
+        Node prev;
+        Node next;
+
+        Node() {
+            this(null, null, null);
+        }
+
+        Node(T item, Node prev, Node next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private final Node sentinel;
+    private int size;
+
+    public LinkedListDeque61B() {
+        sentinel = new Node();
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
+
+    /**
+     * Add {@code x} to the front of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addFirst(T x) {
+        /* before: sentinel <-> first
+            after: sentinel <-> newFirst <-> first */
+        Node first = sentinel.next;
+        Node newFirst = new Node(x, sentinel, first);
+        sentinel.next = newFirst;
+        first.prev = newFirst;
+        size++;
+    }
+
+    /**
+     * Add {@code x} to the back of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addLast(T x) {
+        /* before: last <-> sentinel
+            after: last <-> newFirst <-> sentinel */
+        Node last = sentinel.prev;
+        Node newLast = new Node(x, last, sentinel);
+        sentinel.prev = newLast;
+        last.next = newLast;
+        size++;
+    }
+
+    /**
+     * Returns a List copy of the deque. Does not alter the deque.
+     *
+     * @return a new list copy of the deque.
+     */
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    /**
+     * Returns if the deque is empty. Does not alter the deque.
+     *
+     * @return {@code true} if the deque has no elements, {@code false} otherwise.
+     */
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Returns the size of the deque. Does not alter the deque.
+     *
+     * @return the number of items in the deque.
+     */
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Remove and return the element at the front of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    /**
+     * Remove and return the element at the back of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    /**
+     * The Deque61B abstract data type does not typically have a get method,
+     * but we've included this extra operation to provide you with some
+     * extra programming practice. Gets the element, iteratively. Returns
+     * null if index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    /**
+     * This method technically shouldn't be in the interface, but it's here
+     * to make testing nice. Gets an element, recursively. Returns null if
+     * index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
