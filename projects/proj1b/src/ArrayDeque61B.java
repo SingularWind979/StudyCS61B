b4dc496f9b83b7c3b04de88e09f816a3e55510c9
singularwind <66107716+SingularWind979@users.noreply.github.com>
2026-03-14 13:21:15 +0800

    feat: complete proj1b Task 8 * implement resizeTo with arraycopy * implement smartResize to resize up when absolutely full, resize down when rather empty

--- projects/proj1b/src/ArrayDeque61B.java
+++ projects/proj1b/src/ArrayDeque61B.java
@@ -10,23 +10,24 @@ import java.util.List;
 public class ArrayDeque61B<T> implements Deque61B<T> {
     /** The initial capacity of the deque. */
     private static final int INIT_CAPACITY = 8;
+    private static final int RESIZE_FACTOR = 2;
 
     /** The array that stores the items. */
-    T[] items;
+    private T[] items;
     /** The number of items in the deque. */
-    int size;
+    private int size;
     /**
      * Physical pos in the array
      * where the {@code addFirst} operation will insert the next item.
      * validify(nextFirst + 1) is the first item in the array.
      */
-    int nextFirst;
+    private int nextFirst;
     /**
      * Physical pos in the array
      * where the {@code addLast} operation will insert the next item.
      * validify(nextLast - 1) is the last item in the array.
      */
-    int nextLast;
+    private int nextLast;
 
     /**
      * Constructs an empty ArrayDeque61B.
@@ -64,13 +65,16 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
     }
 
     /**
-     * Add {@code x} to the front of the deque.
+     * Adds {@code x} to the front of the deque.
      * Assumes {@code x} is never null.
      *
      * @param x item to add
      */
     @Override
     public void addFirst(T x) {
+        // Smart resize before adding
+        smartResize();
+
         // Add x to the front of the deque
         items[nextFirst] = x;
         size++;
@@ -80,13 +84,16 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
     }
 
     /**
-     * Add {@code x} to the back of the deque.
+     * Adds {@code x} to the back of the deque.
      * Assumes {@code x} is never null.
      *
      * @param x item to add
      */
     @Override
     public void addLast(T x) {
+        // Smart resize before adding
+        smartResize();
+
         // Add x to the back of the deque
         items[nextLast] = x;
         size++;
@@ -135,7 +142,7 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
     }
 
     /**
-     * Remove and return the element at the front of the deque,
+     * Removes and returns the element at the front of the deque,
      * if it exists.
      *
      * @return removed element, otherwise {@code null}.
@@ -157,11 +164,14 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
         items[nextFirst] = null;
         size--;
 
+        // Smart resize after removing
+        smartResize();
+
         return x;
     }
 
     /**
-     * Remove and return the element at the back of the deque,
+     * Removes and returns the element at the back of the deque,
      * if it exists.
      *
      * @return removed element, otherwise {@code null}.
@@ -182,6 +192,9 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
         // Clear the old position
         items[nextLast] = null;
         size--;
+
+        // Smart resize after removing
+        smartResize();
 
         return x;
     }
@@ -208,7 +221,7 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
 
     /**
      * This method technically shouldn't be in the implementation,
-     * will throw an UnsupportedOperationException.
+     * will throw an {@code UnsupportedOperationException}.
      *
      * @throws UnsupportedOperationException if the method is not implemented
      */
@@ -218,21 +231,62 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
     }
 
     /**
-     * Returns the capacity of the deque.
-     *
-     * @return the capacity of the deque
-     */
-    private int capacity() {
-        return items.length;
-    }
-
-    /**
-     * Returns a valid position in the array.
+     * Validifies the position {@code pos} in the array.
      *
      * @param pos position to validate
      * @return a valid position in the array
      */
     private int validify(int pos) {
-        return Math.floorMod(pos, capacity());
+        return Math.floorMod(pos, items.length);
+    }
+
+    /**
+     * Resizes the array to the new capacity.
+     *
+     * @param newCapacity new capacity of the array
+     */
+    private void resizeTo(int newCapacity) {
+        //noinspection unchecked
+        T[] newItems = (T[]) new Object[newCapacity];
+
+        int head = validify(nextFirst + 1);
+        int tail = validify(nextLast  - 1);
+
+        // Copy elements to the new array
+        if (head <= tail) {
+            System.arraycopy(items, head, newItems, 0, size);
+        } else {
+            int len1 = items.length - head;
+            int len2 = size - len1;
+            System.arraycopy(items, head, newItems, 0, len1);
+            System.arraycopy(items, 0, newItems, len1, len2);
+        }
+
+        // Update the array reference
+        items = newItems;
+
+        // Update nextFirst and nextLast
+        nextFirst = items.length - 1;
+        nextLast = size;
+
+    }
+
+    /**
+     * Smart resize the array.
+     * If the array is absolutely full, resize up by {@code RESIZE_FACTOR}.
+     * If the array is rather empty, resize down by {@code RESIZE_FACTOR}.
+     */
+    private void smartResize() {
+        // absolutely full, resize up by RESIZE_FACTOR
+        if (size == items.length) {
+            resizeTo(items.length * RESIZE_FACTOR);
+        }
+
+        // rather empty, resize down by RESIZE_FACTOR
+        // but not smaller than INIT_CAPACITY
+        if (size < items.length / RESIZE_FACTOR / RESIZE_FACTOR
+                && items.length / RESIZE_FACTOR >= INIT_CAPACITY) {
+            resizeTo(items.length / RESIZE_FACTOR);
+        }
     }
 }
